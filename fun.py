4e80be76614524b2072285d7adbcf452e9df6d20
banjocat <jackmuratore@gmail.com>
2017-02-12 22:15:12 -0500

    first commit

---
+++ fun.py
@@ -0,0 +1,9 @@
+from __future__ import (absolute_import, division,
+        print_function, unicode_literals)
+from buildins import *
+from future import standard_library
+standard_library.install_aliases()
+
+
+
+
